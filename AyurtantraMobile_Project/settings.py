9faf979f6417952797bdd643fe331a8278bda9dc
nirmals <nirmals@quickcap.net>
2020-10-18 01:27:01 +0530

    updated apt file

--- AyurtantraMobile_Project/settings.py
+++ AyurtantraMobile_Project/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'USER': 'sa',
         'PASSWORD': '12345678',
         'OPTIONS': {
-            'driver': 'ODBC Driver 13 for SQL Server',
+            'driver': 'ODBC Driver 17 for SQL Server',
             'unicode_results':True,
         }
     },
