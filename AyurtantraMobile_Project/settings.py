ee065e814f90df3865f3c794f3f8926b189bd9d9
nirmals <nirmals@quickcap.net>
2020-10-27 22:20:55 +0530

    udpated permissions to isauthenticated

--- AyurtantraMobile_Project/settings.py
+++ AyurtantraMobile_Project/settings.py
@@ -221,5 +221,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
